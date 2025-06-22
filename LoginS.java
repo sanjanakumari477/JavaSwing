8def912cbacc70e3afce5abe9ab5726099873fa6
Sanjana Kumari <sksanjana1502@gmail.com>
2025-06-22 16:19:31 +0530

    Create LoginS.java

---
+++ LoginS.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class LoginS
+{
+    public static void main(String[] args) {
+        JFrame f=new JFrame("Hey welcome to java swing");
+        f.setSize(400,400);
+        f.setVisible(true);
+    }
+}
+
+
